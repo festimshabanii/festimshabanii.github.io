f05d02bdec37fe00a12d0539832ae634159adc36
festimshabanii <40381920+festimshabanii@users.noreply.github.com>
2019-09-28 17:55:16 +0200

    d
    d

--- Artisti/scripts.js
+++ Artisti/scripts.js
@@ -13,10 +13,10 @@ var renderer = new THREE.WebGLRenderer({
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-var cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+//var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+//var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+//var cube = new THREE.Mesh( geometry, material );
+//scene.add( cube );
 
 camera.position.z = 700;
 camera.position.x = 1000;
@@ -74,8 +74,8 @@ var animate = function () {
 
 controls.update();
 
-	cube.rotation.x += 0.1;
-	cube.rotation.y += 0.1;
+	//cube.rotation.x += 0.1;
+	//cube.rotation.y += 0.1;
 
 	renderer.render(scene, camera);
 };
